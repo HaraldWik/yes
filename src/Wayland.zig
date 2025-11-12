b3b2aa08fc2fc7a4f7a72ed7633697cd33a7df1d
HaraldWik <wikhallharald@gmail.com>
2025-11-12 19:16:51 +0100

    Fixed wayland so it wont be in the way

--- src/Wayland.zig
+++ src/Wayland.zig
@@ -4,6 +4,20 @@ const root = @import("root.zig");
 // https://nilsbrause.github.io/waylandpp_docs/egl_8cpp-example.html
 // Last supported branch https://github.com/HaraldWik/yes/tree/7699b77785f641670d1b320c3d548466a6eaa4a2 will be re implemented
 
+display: *anyopaque,
+surface: *anyopaque,
+api: struct { opengl: struct {
+    display: *anyopaque,
+    surface: *anyopaque,
+} },
+
+pub const wl = struct {
+    pub fn wl_surface_commit(_: *anyopaque) void {}
+    pub fn wl_display_flush(_: *anyopaque) i32 {
+        return 0;
+    }
+};
+
 pub fn open(config: root.Window.Config) !@This() {
     _ = config;
     return error.NotImplemented;
