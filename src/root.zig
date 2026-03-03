616e9816c147e156a04244519e63dfac10e2f996
HaraldWik <wikhallharald@gmail.com>
2026-03-03 20:22:07 +0100

    Removed legacy

--- src/root.zig
+++ src/root.zig
@@ -2,5 +2,3 @@ const std = @import("std");
 pub const Platform = @import("Platform.zig");
 pub const opengl = @import("opengl.zig");
 pub const vulkan = @import("vulkan.zig");
-
-pub const legacy = @import("legacy/root.zig");
